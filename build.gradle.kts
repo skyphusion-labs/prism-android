a49e82976d422aca01a6145fdb092b696b129e36
Conrad Rockenhaus <conrad@skyphusion.org>
2026-08-07 02:34:31 -0500

    fix(deps): force protobuf 3.25.5 and commons-compress 1.26.0 (#30)

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,11 @@ buildscript {
         "org.bouncycastle:bcutil-jdk18on:1.84",
         "org.bitbucket.b_c:jose4j:0.9.6",
         "org.jdom:jdom2:2.0.6.1",
+        "com.google.protobuf:protobuf-java:3.25.5",
+        "com.google.protobuf:protobuf-kotlin:3.25.5",
+        "com.google.protobuf:protobuf-java-util:3.25.5",
+        "org.apache.commons:commons-compress:1.26.0",
+
       )
     }
   }
@@ -69,6 +74,11 @@ allprojects {
           "org.bouncycastle:bcutil-jdk18on:1.84",
           "org.bitbucket.b_c:jose4j:0.9.6",
           "org.jdom:jdom2:2.0.6.1",
+        "com.google.protobuf:protobuf-java:3.25.5",
+        "com.google.protobuf:protobuf-kotlin:3.25.5",
+        "com.google.protobuf:protobuf-java-util:3.25.5",
+        "org.apache.commons:commons-compress:1.26.0",
+
         )
       }
     }
@@ -92,6 +102,11 @@ allprojects {
         "org.bouncycastle:bcutil-jdk18on:1.84",
         "org.bitbucket.b_c:jose4j:0.9.6",
         "org.jdom:jdom2:2.0.6.1",
+        "com.google.protobuf:protobuf-java:3.25.5",
+        "com.google.protobuf:protobuf-kotlin:3.25.5",
+        "com.google.protobuf:protobuf-java-util:3.25.5",
+        "org.apache.commons:commons-compress:1.26.0",
+
       )
     }
   }
